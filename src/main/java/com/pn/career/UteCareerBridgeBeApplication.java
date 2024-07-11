5ea9ebbe8d91bf1e9aaf8860b52187256e90b20e
Thanh Phong <94186669+thanhphong26@users.noreply.github.com>
2024-07-11 14:25:00 +0700

    initial commit

---
+++ src/main/java/com/pn/career/UteCareerBridgeBeApplication.java
@@ -0,0 +1,13 @@
+package com.pn.career;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UteCareerBridgeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UteCareerBridgeBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pn/career/UteCareerBridgeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pn.career;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UteCareerBridgeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
