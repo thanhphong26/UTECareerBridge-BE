e4674ced88b8a00e3e72094b3de685f9462364eb
Thanh Phong <thanh2652003abc@gmail.com>
2024-08-25 09:22:14 +0700

    CB-01 | Update cors

---
+++ src/main/java/com/pn/career/dtos/EmployerUpdateDTO.java
@@ -0,0 +1,14 @@
+package com.pn.career.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployerUpdateDTO {
+}
